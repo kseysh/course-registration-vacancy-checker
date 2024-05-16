92e0ba58d3a248d2424089abef4a7907d6cf4d27
kseysh <kseysh123@naver.com>
2024-05-16 18:07:13 +0900

    feat: model에 id 추가

--- config/sugang/models.py
+++ config/sugang/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
     day_of_weeks_choices = [
     ('교양선택', 'GENERAL_ELECTIVE'),
     ('교양필수', 'GENERAL_REQUIRED'),
@@ -14,12 +15,14 @@ class Subject(models.Model):
         return self.subject
 
 class Major(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(verbose_name="학부 전공", max_length=20, default="전공")
 
     def __str__(self):
         return self.name
 
 class CourseTime(models.Model):
+    id = models.AutoField(primary_key=True)
     day_of_weeks_choices = [
     ('월', 'MONDAY'),
     ('화', 'TUESDAY'),
@@ -37,6 +40,7 @@ class CourseTime(models.Model):
         return self.day_of_week + "요일 " + str(self.period) + "교시"
 
 class Course(models.Model):
+    id = models.AutoField(primary_key=True)
     code = models.CharField(verbose_name="학수번호", max_length=15, unique = True)
     name = models.CharField(verbose_name="과목명", max_length=40)
     grade = models.CharField(verbose_name="학년", max_length=10, default="0")
