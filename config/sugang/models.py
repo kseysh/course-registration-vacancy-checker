7cc6057703d9fabc6cd6e2b008323f4675063c00
kseysh <kseysh123@naver.com>
2024-05-16 17:43:14 +0900

    feat: model 수정 및 admin 적용

--- config/sugang/models.py
+++ config/sugang/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 class Subject(models.Model):
-    subject = models.CharField(verbose_name="과목구분 id", max_length=4, default="정보 없음")
+    day_of_weeks_choices = [
+    ('교양선택', 'GENERAL_ELECTIVE'),
+    ('교양필수', 'GENERAL_REQUIRED'),
+    ('전공선택', 'MAJOR_ELECTIVE'),
+    ('전공필수', 'MAJOR_REQUIRED'),
+    ('미정', 'UNDETERMINED'),
+    ]
+    subject = models.CharField(verbose_name="과목구분 id", max_length=4, default ="미정")
+
+    def __str__(self):
+        return self.subject
 
 class Major(models.Model):
     name = models.CharField(verbose_name="학부 전공", max_length=20, default="전공")
+
+    def __str__(self):
+        return self.name
 
 class CourseTime(models.Model):
     day_of_weeks_choices = [
@@ -14,10 +27,14 @@ class CourseTime(models.Model):
     ('목', 'THURSDAY'),
     ('금', 'FRIDAY'),
     ('토', 'SATURDAY'),
-    ('일', 'SUNDAY')
-]
-    day_of_week = models.CharField(verbose_name="강의 요일", choices= day_of_weeks_choices, max_length=8, default="?")
-    period = models.SmallIntegerField(verbose_name="교시")
+    ('일', 'SUNDAY'),
+    ('미정', 'UNDETERMINED'),
+    ]
+    day_of_week = models.CharField(verbose_name="강의 요일", choices= day_of_weeks_choices, max_length=8, default="미정")
+    period = models.SmallIntegerField(verbose_name="교시", default=0)
+
+    def __str__(self):
+        return self.day_of_week + "요일 " + str(self.period) + "교시"
 
 class Course(models.Model):
     code = models.CharField(verbose_name="학수번호", max_length=15, unique = True)
@@ -25,10 +42,15 @@ class Course(models.Model):
     grade = models.CharField(verbose_name="학년", max_length=10, default="0")
     credit = models.DecimalField(verbose_name="학점", max_digits=2, decimal_places=1, default= 0.0)
     
-    time = models.CharField(verbose_name="시간", max_length=256, default="")
+    evaluation_method_type = [
+    ('절대평가', 'ABSOLUTE'),
+    ('상대평가', 'RELATIVE'),
+    ('미정', 'UNDETERMINED'),
+    ]
+
     classroom = models.CharField(verbose_name="강의실", max_length=256, default="")
     professor = models.CharField(verbose_name="담당교수", max_length=10, default="담당교수 미정")
-    evaluation_method = models.CharField(verbose_name="평가방식", max_length=10, default="")
+    evaluation_method = models.CharField(verbose_name="평가방식", choices=evaluation_method_type, max_length=10, default="미정")
     remarks = models.CharField(verbose_name="비고", max_length=10, default="")
 
     subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
@@ -37,3 +59,6 @@ class Course(models.Model):
 
     enrollment_count = models.IntegerField(verbose_name="신청 인원수", default=0)
     enrollment_capacity = models.IntegerField(verbose_name="신청 정원", default=0)    
+
+    def __str__(self):
+        return self.name
