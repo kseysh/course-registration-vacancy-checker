f25b74ad85839df76a3a330c7b9a85cdb2ead232
kseysh <kseysh123@naver.com>
2024-07-24 19:15:27 +0900

    feat: 강의 정보 크롤링 코드 작성

--- config/crawler/views.py
+++ config/crawler/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from sugang.models import *
+from selenium.webdriver.support.select import Select
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from .crawled_info.majors_list import majors
 
-# Create your views here.
+def crawl_course_info(request):
+    service = Service("/Users/transfer_kk/Desktop/chromedriver-mac-arm64/chromedriver")
+    driver = webdriver.Chrome(service=service)
+    driver.implicitly_wait(3)
+    
+    url = 'https://sugang.inha.ac.kr/sugang/SU_51001/Lec_Time_Search.aspx?callPage=Sugang_SaveAB'
+    driver.get(url)
+    
+    for major in majors:
+        select = Select(driver.find_element(By.NAME, 'ddlDept'))
+        major_value = major["value"]
+        select.select_by_value(major_value)
+        driver.find_element(By.CSS_SELECTOR, "#ibtnSearch1").click()
+        driver.implicitly_wait(3)
+            
+        # ------------ 아 -----------
+        
+        rows = driver.find_elements(By.CSS_SELECTOR, '#dgList > tbody > tr')
+
+        for row in rows:
+            code = row.find_element(By.CSS_SELECTOR, "td:nth-child(1) > a > font").text.strip() # 학수번호
+            name = row.find_element(By.CSS_SELECTOR, "td:nth-child(3)").text.strip() # 과목명
+            grade = row.find_element(By.CSS_SELECTOR, "td:nth-child(4)").text.strip() # 학년
+            credit = row.find_element(By.CSS_SELECTOR, "td:nth-child(5)").text.strip() # 학점
+            subject = row.find_element(By.CSS_SELECTOR, "td:nth-child(6)").text.strip() # 과목 구분
+            time_and_classroom = row.find_element(By.CSS_SELECTOR, "td:nth-child(7)").text.strip() # 시간 및 강의실
+            professor = row.find_element(By.CSS_SELECTOR, "td:nth-child(8)").text.strip() # 교수
+            evaluation_method = row.find_element(By.CSS_SELECTOR, "td:nth-child(9)").text.strip() # 평가 방식
+            remarks = row.find_element(By.CSS_SELECTOR, "td:nth-child(10)").text.strip() # 비고
+            
+            try:
+                course, course_is_created = Course.objects.get_or_create(
+                    code = code,
+                    name = name,
+                    grade = grade,
+                    credit = credit,
+                    subject = subject,
+                    time_and_classroom = time_and_classroom,
+                    professor = professor,
+                    evaluation_method = evaluation_method,
+                    remarks = remarks,
+                )
+            except:
+                print("course code" + course.code, "cource name " + course.name, "major value" + major_value)
+            
+            if course_is_created:
+                print(course.name)
+
+    return HttpResponse("success!")
--- config/sugang/models.py
+++ config/sugang/models.py
@@ -10,23 +10,10 @@ class Course(models.Model):
     professor = models.CharField(verbose_name="담당교수", max_length=10, default="담당교수 미정")
     remarks = models.CharField(verbose_name="비고", max_length=10, default="")
     vacancy = models.CharField(verbose_name="여석", max_length=200, default="미정")
+    time_and_classroom = models.CharField(verbose_name="시간 및 강의실", max_length=200, default="미정")
     major_name = models.CharField(verbose_name="전공 이름", max_length=20, default="미정")
-
-    evaluation_method_type = [
-    ('절대평가', 'ABSOLUTE'),
-    ('상대평가', 'RELATIVE'),
-    ('미정', 'UNDETERMINED'),
-    ]
-    evaluation_method = models.CharField(verbose_name="평가방식", choices=evaluation_method_type, max_length=10, default="미정")
-
-    day_of_weeks_choices = [
-    ('교양선택', 'GENERAL_ELECTIVE'),
-    ('교양필수', 'GENERAL_REQUIRED'),
-    ('전공선택', 'MAJOR_ELECTIVE'),
-    ('전공필수', 'MAJOR_REQUIRED'),
-    ('미정', 'UNDETERMINED'),
-    ]
-    subject = models.CharField(verbose_name="과목구분", choices=day_of_weeks_choices, max_length=5, default="미정")
+    evaluation_method = models.CharField(verbose_name="평가방식", max_length=10, default="미정")
+    subject = models.CharField(verbose_name="과목구분", max_length=5, default="미정")
     
     def __str__(self):
-        return self.name
+        return self.code + "/" + self.name
