dc83708bd4513a60b3b09ad1facacd5f9785e146
kseysh <kseysh123@naver.com>
2025-01-22 16:23:45 +0900

    chore: 학과 변경 반영

--- config/crawler/major_creator.py
+++ config/crawler/major_creator.py
@@ -1,7 +1,9 @@
 import json
 
 # 주어진 HTML 데이터
+
 html_data = """
+	<option value="1206574">법학과목 / 법학과목</option>
 	<option value="0194002">기계공학과 / 기계공학</option>
 	<option value="0198005">항공우주공학과 / 항공우주공학</option>
 	<option value="1013446">조선해양공학과 / 조선해양공학</option>
@@ -17,11 +19,12 @@ html_data = """
 	<option value="0691156">건축학부 / 건축공학</option>
 	<option value="0691305">건축학부 / 건축학</option>
 	<option value="0930430">에너지자원공학과 / 에너지자원공학</option>
-	<option value="0208014">전기공학과 / 전기공학</option>
-	<option value="0209015">전자공학과 / 전자공학</option>
-	<option value="1185283">정보통신공학과 / 정보통신공학</option>
-	<option value="1185767">정보통신공학과 / 인공지능반도체공학</option>
+	<option value="1601284">전기전자공학부 / 전기전자공학</option>
+	<option value="1601767">전기전자공학부 / 인공지능반도체공학</option>
+	<option value="1602285">이차전지융합학과 / 이차전지융합학</option>
+	<option value="1602756">이차전지융합학과 / 이차전지공학</option>
 	<option value="1443732">공과대학(FVE) / 미래자동차공학</option>
+	<option value="1565408">반도체시스템공학과 / 반도체</option>
 	<option value="1565770">반도체시스템공학과 / 반도체시스템공학</option>
 	<option value="1063157">수학과 / 수학</option>
 	<option value="1064040">통계학과 / 통계학</option>
@@ -29,8 +32,8 @@ html_data = """
 	<option value="1066041">화학과 / 화학</option>
 	<option value="1023307">해양과학과 / 해양과학</option>
 	<option value="1186074">식품영양학과 / 식품영양학</option>
-	<option value="0333045">경영학과 / 경영학</option>
-	<option value="1025431">글로벌금융학과 / 글로벌금융학</option>
+	<option value="1607045">경영학부 경영학과 / 경영학</option>
+	<option value="1608093">경영학부 파이낸스경영학과 / 파이낸스경영학</option>
 	<option value="0934339">아태물류학부 / 물류학</option>
 	<option value="1187268">국제통상학과 / 국제통상학</option>
 	<option value="0267059">국어교육과 / 국어교육</option>
@@ -52,19 +55,18 @@ html_data = """
 	<option value="0302072">철학과 / 철학</option>
 	<option value="1192556">중국학과 / 중국학</option>
 	<option value="1028485">일본언어문화학과 / 일본언어문화</option>
-	<option value="0294067">영어영문학과 / 영어영문학</option>
-	<option value="1029486">프랑스언어문화학과 / 프랑스언어문화</option>
+	<option value="1604067">영미유럽인문융합학부 / 영어영문학</option>
+	<option value="1604088">영미유럽인문융합학부 / 영미유럽인문융합학부</option>
+	<option value="1604486">영미유럽인문융합학부 / 프랑스언어문화</option>
 	<option value="1236588">문화콘텐츠문화경영학과 / 문화콘텐츠문화경영학</option>
 	<option value="0317077">의예과 / 의예</option>
-	<option value="0321079">간호학과 / 간호학</option>
 	<option value="0318078">의학과 / 의학</option>
+	<option value="1600079">간호학과 / 간호학</option>
 	<option value="1238590">소프트웨어융합공학과 / 소프트웨어융합공학</option>
 	<option value="1239591">산업경영학과 / 산업경영학</option>
 	<option value="1262600">메카트로닉스공학과 / 메카트로닉스공학</option>
 	<option value="1263601">금융투자학과 / 금융투자학</option>
 	<option value="1564769">반도체산업융합학과 / 반도체산업융합</option>
-	<option value="1207569">프런티어학부대학 / 프런티어학부대학</option>
-	<option value="1519763">자유전공학부 / 자유전공학부</option>
 	<option value="1393686">IBT학과 / 국제경영학</option>
 	<option value="1394687">ISE학과 / 융합시스템공학</option>
 	<option value="1395695">KLC학과 / 국제한국언어문화학</option>
@@ -74,6 +76,7 @@ html_data = """
 	<option value="1391724">스마트모빌리티공학과 / 스마트모빌리티공학</option>
 	<option value="1392725">디자인테크놀로지학과 / 디자인테크놀로지학</option>
 	<option value="1223583">소프트웨어융합대학(SCSC) / 연계전공(SCSC)</option>
+	<option value="1461344">예술체육대학 / 예술체육학부</option>
 	<option value="1449558">조형예술학과 / 조형예술학</option>
 	<option value="1450589">디자인융합학과 / 디자인융합학</option>
 	<option value="1452519">스포츠과학과 / 스포츠과학</option>
@@ -82,6 +85,14 @@ html_data = """
 	<option value="1581304">생명공학과 / 생명공학</option>
 	<option value="1582306">생명과학과 / 생명과학</option>
 	<option value="1583737">바이오제약공학과 / 바이오제약공학</option>
+	<option value="1603353">첨단바이오의약학과 / 첨단바이오의약학</option>
+	<option value="1613354">자유전공융합학부 / 자유전공융합학부</option>
+	<option value="1614096">공학융합학부 / 공학융합학부</option>
+	<option value="1615102">자연과학융합학부 / 자연과학융합학부</option>
+	<option value="1616110">경영융합학부 / 경영융합학부</option>
+	<option value="1617262">사회과학융합학부 / 사회과학융합학부</option>
+	<option value="1618265">인문융합학부 / 인문융합학부</option>
+	<option value="1631477">프런티어창의대학 / 프런티어창의대학</option>
 """
 
 # 문자열 처리
@@ -109,4 +120,5 @@ print("\n\n------------------\n\n")
 
 majors_list = json.loads(majors_json)
 
-print({major['value']: major['name'] for major in majors_list})
+for major in majors_list:
+    print(f"'{major['value']}': '{major['name']} / {major['name']}',")
